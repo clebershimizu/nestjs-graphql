aaf4d63904151afffbbef34aabc6345385313aef
clebershimizu <cleber.shimizu@gmail.com>
2022-03-27 19:06:00 -0300

    finished project

---
+++ src/users/users.resolver.ts
@@ -0,0 +1,36 @@
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { GetUserArgs } from './dto/args/get-user.args';
+import { GetUsersArgs } from './dto/args/get-users.args';
+import { CreateUserInput } from './dto/input/create-user.input';
+import { DeleteUserInput } from './dto/input/delete-user.input';
+import { UpdateUserInput } from './dto/input/update-user.input';
+import { User } from './models/user';
+import { UsersService } from './users.service';
+
+@Resolver(() => User)
+export class UsersResolver {
+  constructor(private readonly _usersService: UsersService) {}
+
+  @Query(() => User, { name: 'user', nullable: true })
+  getUser(@Args() getUserArgs: GetUserArgs): User {
+    return this._usersService.getUser(getUserArgs);
+  }
+
+  @Query(() => [User], { name: 'users', nullable: 'items' })
+  getUsers(@Args() getUsersArgs: GetUsersArgs): User[] {
+    return this._usersService.getUsers(getUsersArgs);
+  }
+  @Mutation(() => User)
+  creteUser(@Args('createUserData') createUserData: CreateUserInput): User {
+    return this._usersService.createUser(createUserData);
+  }
+
+  @Mutation(() => User)
+  updateUser(@Args('updateUserData') updateUserData: UpdateUserInput): User {
+    return this._usersService.updateUser(updateUserData);
+  }
+  @Mutation(() => User)
+  deleteUser(@Args('deleteUserData') deleteUserData: DeleteUserInput): User {
+    return this._usersService.deleteUser(deleteUserData);
+  }
+}
